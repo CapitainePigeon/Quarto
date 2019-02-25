e95b309c8432d72b5e9304058b1b06cbf7ebe171
paulinermbd <p.rambaud.pr@gmail.com>
2019-02-25 12:02:16 +0100

    Classe pièce ajoutée

---
+++ include/piece.h
@@ -0,0 +1,21 @@
+#ifndef PIECE_H
+#define PIECE_H
+
+
+class piece
+{
+    public:
+        piece();
+        virtual ~piece();
+
+
+    protected:
+
+    private:
+        bool couleur;
+        bool taille;
+        bool forme;
+        bool pleine;
+};
+
+#endif // PIECE_H
