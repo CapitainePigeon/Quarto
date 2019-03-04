3668f83ac910febb3bdfba1d6f6f623dfcd493c6
CapitainePigeon <37774136+CapitainePigeon@users.noreply.github.com>
2019-03-04 09:03:03 +0100

    coucou

---
+++ include/ReserveDePiece.h
@@ -0,0 +1,16 @@
+#ifndef RESERVEDEPIECE_H
+#define RESERVEDEPIECE_H
+
+
+class ReserveDePiece
+{
+    public:
+        ReserveDePiece();
+        virtual ~ReserveDePiece();
+
+    protected:
+
+    private:
+};
+
+#endif // RESERVEDEPIECE_H
